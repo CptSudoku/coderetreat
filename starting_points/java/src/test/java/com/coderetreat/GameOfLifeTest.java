29d9ddbdcfa983cac8bbc2ef7e63341783718644
Bogdan Azaric <bogdan.azaric@sbgenomics.com>
2017-12-27 18:05:33 +0100

    Merge[DOJO] - initial commit

--- starting_points/java/src/test/java/com/coderetreat/GameOfLifeTest.java
+++ starting_points/java/src/test/java/com/coderetreat/GameOfLifeTest.java
@@ -2,12 +2,10 @@ package com.coderetreat;
 
 import org.junit.Test;
 
-import static org.fest.assertions.Assertions.assertThat;
-
 public class GameOfLifeTest {
 
     @Test
     public void trueShouldBeTrue() {
-        assertThat(true).isEqualTo(false);
+//        assertThat(true).isEqualTo(false);
     }
 }
